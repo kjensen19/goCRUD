feeff5718e11167ca8e6755b7c5d75ea85907cc8
Kyle Jensen <kjensen19@gmail.com>
2023-01-10 15:25:11 -0600

    the struggle is real

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"database/sql"
+	// "database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -18,82 +18,110 @@ type task struct {
 	Status      string `json:"status"`
 }
 
-var tasks = []task{
-	{ID: "1", Name: "Build APIs", Description: "REST APIs", Assigned: "Kyle", Status: "In Progress"},
-	{ID: "2", Name: "Build DB connection", Description: "Link with Postgres", Assigned: "Kyle", Status: "ToDo"},
-	{ID: "3", Name: "Frontend", Description: "Style the boards", Assigned: "Kyle", Status: "Someday"},
-}
+// var tasks = []task{
+// 	{ID: "1", Name: "Build APIs", Description: "REST APIs", Assigned: "Kyle", Status: "In Progress"},
+// 	{ID: "2", Name: "Build DB connection", Description: "Link with Postgres", Assigned: "Kyle", Status: "ToDo"},
+// 	{ID: "3", Name: "Frontend", Description: "Style the boards", Assigned: "Kyle", Status: "Someday"},
+// }
 
 func main() {
-	Dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
 		os.Exit(1)
 	}
-	defer Dbpool.Close()
+	defer dbpool.Close()
 
-	router := gin.Default(),
-
-
-	router.GET("/tasks", getTasks)
-	router.GET("/tasks/:id", getTask)
-	router.POST("/tasks", addTask)
-
-	router.Run("localhost:8080")
-}
-
-func masterTask(dbpool *pgxpool.Pool)
-	 task, err := getTaskDB(dbpool)
-
-func getTasks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, tasks)
-
-}
-
-func getTaskDB(dbpool *pgxpool.Pool) ([]task, error) {
-	var tasks []task
-
-	rows, err := dbpool.Query(context.Background(), "select * from tasks")
+	var greeting string
+	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
 	if err != nil {
-		return nil, fmt.Errorf("GET all albums failed: %v", err)
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(7)
 	}
-	defer rows.Close()
-	for rows.Next() {
-		var tas task
-		if err := rows.Scan(&tas.ID, &tas.Name, &tas.Description, &tas.Assigned, &tas.Status); err != nil {
-			return nil, fmt.Errorf("GET all albums failed: %v", err)
+	fmt.Printf(greeting)
+	router := gin.Default()
+
+	// router.GET("/tasks/:id", getTask)
+	// router.POST("/tasks", addTask)
+	router.GET("/tasks", getHandler(dbpool))
+	router.Run(":8080")
+
+}
+
+func getHandler(dbpool *pgxpool.Pool) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		var tasks []task
+
+		rows, err := dbpool.Query(context.Background(), "SELECT * from task")
+		c.IndentedJSON(http.StatusOK, tasks)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "GET all tasks failed here: %v", err)
+			os.Exit(2)
 		}
-		tasks = append(tasks, tas)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("GET all albums failed: %v", err)
-	}
-	return tasks, nil
-}
-
-func getTask(c *gin.Context) {
-	id := c.Param("id")
-
-	for _, t := range tasks {
-		if t.ID == id {
-			c.IndentedJSON(http.StatusOK, t)
-			return
+		defer rows.Close()
+		for rows.Next() {
+			var tas task
+			if err := rows.Scan(&tas.ID, &tas.Name, &tas.Description, &tas.Assigned, &tas.Status); err != nil {
+				fmt.Fprintf(os.Stderr, "GET all tasks failed: %v", err)
+				os.Exit(3)
+			}
+			tasks = append(tasks, tas)
 		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "task not found"})
-}
+		c.IndentedJSON(http.StatusOK, tasks)
+		// if err := rows.Err(); err != nil {
+		// 	fmt.Fprintf(os.Stderr, "GET all tasks failed: %v", err)
+		// 	os.Exit(4)
+		// }
 
-func addTask(c *gin.Context) {
-	var newTask task
-
-	if err := c.BindJSON(&newTask); err != nil {
-		return
 	}
 
-	tasks = append(tasks, newTask)
-	c.IndentedJSON(http.StatusCreated, newTask)
-
+	return gin.HandlerFunc(fn)
 }
+
+// func getTaskDB(dbpool *pgxpool.Pool) ([]task, error) {
+// 	var tasks []task
+
+// 	rows, err := dbpool.Query(context.Background(), "select * from task")
+// 	if err != nil {
+// 		return nil, fmt.Errorf("GET all albums failed: %q %v", rows, err)
+// 	}
+// 	defer rows.Close()
+// 	for rows.Next() {
+// 		var tas task
+// 		if err := rows.Scan(&tas.ID, &tas.Name, &tas.Description, &tas.Assigned, &tas.Status); err != nil {
+// 			return nil, fmt.Errorf("GET all albums failed: %v", err)
+// 		}
+// 		tasks = append(tasks, tas)
+// 	}
+// 	if err := rows.Err(); err != nil {
+// 		return nil, fmt.Errorf("GET all albums failed: %v", err)
+// 	}
+// 	return tasks, nil
+// }
+
+// func getTask(c *gin.Context) {
+// 	id := c.Param("id")
+
+// 	for _, t := range tasks {
+// 		if t.ID == id {
+// 			c.IndentedJSON(http.StatusOK, t)
+// 			return
+// 		}
+// 	}
+// 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "task not found"})
+// }
+
+// func addTask(c *gin.Context) {
+// 	var newTask task
+
+// 	if err := c.BindJSON(&newTask); err != nil {
+// 		return
+// 	}
+
+// 	tasks = append(tasks, newTask)
+// 	c.IndentedJSON(http.StatusCreated, newTask)
+
+// }
 
 //GET all Request syntax:
 // curl http://localhost:8080/tasks \
