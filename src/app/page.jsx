b9852dc4f9432a763e312e46f6831df5c01259c4
Kyle Jensen <kjensen19@gmail.com>
2023-01-14 01:30:07 -0600

    server issues continue

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,2 +1,25 @@
-import getTask from "@/pages/getTask";
+import GetTask from "@/pages/getTask";
 
+export default async function Page() {
+  var task = []
+  task = GetTask()
+  console.log('?', task)
+  // function handleClick(){
+  //   task = GetTask()
+  //   console.log(task)
+  //   return task
+  // }
+
+  return(
+    <>
+      <h1>Is it working?</h1>
+      <button>ttt</button>
+      <ul>
+        {task.length !== 0 && task.map((dat,i) =>(
+            <li key={i}>{dat.name}</li>
+          )
+        )}
+    </ul>
+    </>
+  )
+}
--- src/pages/getTask.jsx
+++ src/pages/getTask.jsx
@@ -1,12 +1,19 @@
 
-export default function getTask() {
+
     const task = async () => {
-        try {
-            const res = await fetch('https://localhost/8080/tasks');
+        try { 
+            const res = await fetch(`http://localhost:8080/tasks`);
             const data = await res.json();
-            console.log(data);
+            console.log('data?', data);
+            return data
         } catch (err) {
             console.log(err);
         }
+       
     };
-};
+
+
+export default async function GetTask() {
+   const tasks = await task()
+   return tasks
+}
