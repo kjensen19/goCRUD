a1dde95dfda6430bf6d5567b78a9d1342d487f44
Kyle Jensen <kjensen19@gmail.com>
2023-01-14 22:32:18 -0600

    app modifications for list of tasks

--- gofront/src/App.js
+++ gofront/src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [taskList, setTaskList] = useState('')
   const testConn = () => {
     axios({
       method: 'GET',
       url: 'http://localhost:8080/tasks'
     }).then((res) => {
       console.log('res', res.data)
+      setTaskList(res.data)
     }).catch((err) => {
       console.log('GET err: ', err)
     })
+    
   }
 
 
@@ -19,6 +23,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button onClick={testConn}>?????</button>
+        <ul>
+          {taskList && taskList.map((task,i) =>(
+            <li key={i}>{[task.name, task.description, task.assigned, task.status]}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
